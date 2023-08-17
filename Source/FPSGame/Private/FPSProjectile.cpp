3b7f04100798f4d3d577a3b508a9208920fe291a
eggmaker <1402461065@qq.com>
2023-08-17 23:14:35 +0800

    Stealth Game

--- Source/FPSGame/Private/FPSProjectile.cpp
+++ Source/FPSGame/Private/FPSProjectile.cpp
@@ -29,6 +29,9 @@ AFPSProjectile::AFPSProjectile()
 
 	// Die after 3 seconds by default
 	InitialLifeSpan = 3.0f;
+
+	SetReplicates(true);
+	SetReplicateMovement(true);
 }
 
 
@@ -38,7 +41,9 @@ void AFPSProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPr
 	if ((OtherActor != NULL) && (OtherActor != this) && (OtherComp != NULL) && OtherComp->IsSimulatingPhysics())
 	{
 		OtherComp->AddImpulseAtLocation(GetVelocity() * 100.0f, GetActorLocation());
-
-		Destroy();
 	}
+
+	MakeNoise(1.0f, Instigator);
+
+	Destroy();
 }
